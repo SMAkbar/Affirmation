c82d50603b64e8050e4a68585df741a0db3484ea
SMAkbar <sm.akbar42@gmail.com>
2021-10-06 17:36:18 +0500

    App completed after adding images, encompassing everything within card view and add launcher icon

--- app/src/main/java/com/akbar/affirmation/adapter/Itemadapter.kt
+++ app/src/main/java/com/akbar/affirmation/adapter/Itemadapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.akbar.affirmation.R
@@ -12,8 +13,10 @@ import com.akbar.affirmation.model.Affirmation
 class Itemadapter (private val context : Context,
                    private val dataset : List<Affirmation>)
     : RecyclerView.Adapter<Itemadapter.ItemViewHolder>() {
-    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
-        val textView : TextView = view.findViewById(R.id.item_title)
+
+    class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -24,6 +27,7 @@ class Itemadapter (private val context : Context,
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     override fun getItemCount() = dataset.size
